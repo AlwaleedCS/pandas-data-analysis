e78873cd7fb9b18a7e753cc6732e13d97f1415f7
Alwaleed Alahmadi <AlwaleedAlahmadi@outlook.com>
2026-06-25 01:09:24 +0300

    Add initial pandas data analysis script

---
+++ src/pandas_data_analysis.py
@@ -0,0 +1,14 @@
+﻿import pandas as pd
+
+data = {
+    'Major': ['IT', 'CS', 'MIS', 'Nan', 'CS', 'MIS', 'Nan', 'CS', 'Nan'],
+    'Course Name': ['Introduction to IT', 'Programming Fundamentals', 'Introduction to MIS', 'Database Management', 'Data Structures', 'Systems Analysis and Design', 'Network Security', 'Algorithms', 'Nan'],
+    'Level': [100, 100, 100, 200, 'Nan', 200, 300, 300, 'Nan']
+}
+
+df = pd.DataFrame(data)    
+print(df.describe())        
+df['Level'] = df['Level'].astype(float)
+print(df.head(4))
+df = df.dropna(how='all')
+print(df)
